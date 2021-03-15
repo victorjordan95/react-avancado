92f034163591c46973881d3e138ee7c70aea5125
Victor Jordan <victorjordan@fitec.org.br>
2021-03-15 18:35:32 -0300

    Aula #25 - Atualizando story book

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,15 +1,13 @@
 import { Meta, Story } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 const MainSetting: Meta = {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
 };
 
-export const Basic: Story = () => (
-  <Main title={text('Title', 'React Avançado')} />
+export const Basic: Story = (args) => (
+  <Main {...args} />
 );
 
 export default MainSetting;
